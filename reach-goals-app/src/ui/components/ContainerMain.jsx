488b4fc7a888880dbb61f16ec839c215a3f18045
Lucas <153150990+LukasO20@users.noreply.github.com>
2024-09-14 16:17:21 -0300

    New structures to Principal Container and Home
    - Added new styles structure to App.scss and Home.scss
    - Build more structure of HTML

--- reach-goals-app/src/ui/components/ContainerMain.jsx
+++ reach-goals-app/src/ui/components/ContainerMain.jsx
@@ -3,5 +3,36 @@ import Routes from '../../app/Routes'
 
 export default props => 
     <div className="container-main">
-        <Routes />
+        <div className="head">
+            <div className="line-p">
+                <div className="title-m">
+                    <h2>Your control panel</h2>
+                </div>
+                <div className="options-m">
+                    <div className="visibility-m">
+                        <label className="visibility button-m">visibility</label>
+                    </div>
+                    <div className="more-m">
+                        <label className="more button-m"><i className="fa-solid fa-ellipsis-vertical"></i></label>
+                    </div>
+                </div>
+            </div>
+            <div className="line-s">
+                <div className="filter">
+                    <label className="checkbox button-filter-m"></label>
+                    <label className="filter-content button-filter-m"><i className="icon-st fa-solid fa-filter"></i></label>
+                    <label className="line-chart button-filter-m"><i className="icon-st fa-solid fa-chart-line"></i></label>
+                    <label className="search button-filter-m"><i className="icon-st fa-solid fa-magnifying-glass"></i><input type="text" placeholder="search" id="search-content-m" className="search-content" /></label>
+                    <label className="goal button-filter-m button-st">goals</label>
+                    <label className="assignment button-filter-m button-st">unfocused assignments</label>
+                </div>
+                <div className="action">
+                    <label className="goal button-action-m button-st"><i className="icon-st fa-solid fa-sort"></i>order</label>
+                    <label className="goal button-action-m button-st"><i className="icon-st fa-solid fa-plus"></i>create</label>
+                </div>
+            </div>
+        </div>
+        <div className="body">
+            <Routes />
+        </div>
     </div>
--- reach-goals-app/src/ui/components/Home.jsx
+++ reach-goals-app/src/ui/components/Home.jsx
@@ -1,5 +1,23 @@
 import React from 'react'
+import '../styles/Home.scss'
 
 export default props => 
     <div className="container-home">
+        <div className="itens">
+            <div className="itens-to-do column">
+                <div className="head-column"></div>
+                <div className="body-column"></div>
+            </div>
+            <div className="itens-progress column">
+                <div className="head-column"></div>
+                <div className="body-column"></div>
+            </div>
+            <div className="itens-finished column">
+                <div className="head-column"></div>
+                <div className="body-column"></div>
+            </div>
+        </div>
+        <div className="chart-line">
+            <h1>Representation chart line here...</h1>
+        </div>
     </div>
