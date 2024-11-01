91921693792029f05e187990bc52377f14b394e1
Lucas <153150990+LukasO20@users.noreply.github.com>
2024-11-01 00:34:55 -0300

    Added more structure layout to config panel - 'Config.jsx' and 'Config.scss''
    - New styles CSS to config panel
    - New structures HTML to config panel

--- reach-goals-app/src/ui/components/panels/Config.jsx
+++ reach-goals-app/src/ui/components/panels/Config.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Button from '../items/elements/Button'
+import '../../styles/panels/Config.scss'
 
 const targetMap = (classes, operator = {}) => {
     const data = Array.isArray(classes) ? classes : [classes]
@@ -14,12 +15,24 @@ const Config = () => {
     return (
         <div className="container-config center-content">
             <div className='head'>
-                <h2>Configurations</h2>
+                <h2><i className='icon-st fa-solid fa-sliders'></i>Configurations</h2>
                 <Button target={targetMap(null)} classBtn='button-action-p close-modal button-st' iconFa='fa-solid fa-xmark'/>
-                </div>
+            </div>
             <div className='body'>
                 <div className='aside-config'>
-
+                    <div className='nav-itens'>
+                        <div className='head'>
+                            <label className='button-filter-m search'>
+                                <i className='icon-st fa-solid fa-magnifying-glass'></i><input type='text' placeholder='search' id='search-content-m' className='search-content'/>
+                            </label>
+                        </div>
+                        <div className='body'>
+                            <Button classBtn='button-st button-action-m config-not' title='Notifications' iconFa='fa-solid fa-bell'/>
+                            <Button classBtn='button-st button-action-m config-theme' title='Themes' iconFa='fa-solid fa-palette'/>
+                            <Button classBtn='button-st button-action-m config-search' title='Searched items' iconFa='fa-solid fa-list'/>
+                            <Button classBtn='button-st button-action-m config-layout' title='Layout' iconFa='fa-solid fa-layer-group'/>
+                        </div>
+                    </div>
                 </div>
                 <div className='section-options-config'>
 
