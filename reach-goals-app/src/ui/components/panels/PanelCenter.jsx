0e27e8ed2719f89cd02d0e5f274fa158a2f5f326
Lucas <153150990+LukasO20@users.noreply.github.com>
2024-11-15 17:10:23 -0300

    Initial commit to server (server.js) to DBA structure
    - Add simple DBA (Data base application on vercel server)
    - Initial commit to server's structure

--- reach-goals-app/src/ui/components/panels/PanelCenter.jsx
+++ reach-goals-app/src/ui/components/panels/PanelCenter.jsx
@@ -3,22 +3,12 @@ import { VisibilityContext } from '../../../provider/components/VisibilityProvid
 import ModalForm from './ModalForm'
 import Config from './Config'
 
-const targetMap = (classes) => {
-    const data = Array.isArray(classes) ? classes : [classes]
-    const attributes = {
-        class: data
-    }
-    return attributes
-}
-
 const PanelCenter = (props) => {
-    const { visibleElements, toggleVisibility } = useContext(VisibilityContext)
+    const { visibleElements } = useContext(VisibilityContext)
 
     const btnCurrent = visibleElements[1] ?? ''
     const typeForm = btnCurrent.split(' ')[0]
-    
-    // Used to remove target childrens
-    //const classRemove = visibleElements.length > 2 ? visibleElements.slice(2) : visibleElements.slice(0, 2)
+
     return (
         <div className={`content-center ${visibleElements.includes(props.id) ? 'show' :  ''} ${typeForm}`}>
             <Config />
