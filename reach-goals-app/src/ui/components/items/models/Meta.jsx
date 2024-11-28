acd4afd3f7c081af0c8369095e75e0411459bcb5
Lucas <153150990+LukasO20@users.noreply.github.com>
2024-11-27 23:51:21 -0300

    Added new structure to 'Meta.jsx' component and some little improvements on 'Home.jsx' component
    - Little changes on 'Home.jsx' component
    - New structure to 'Meta.jsx'

--- reach-goals-app/src/ui/components/items/models/Meta.jsx
+++ reach-goals-app/src/ui/components/items/models/Meta.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
-import * as metaAction from '../../../provider/meta/metaAction'
+import { useEffect, useState } from 'react'
+import * as metaAction from '../../../../provider/meta/metaAction'
 
-const listMeta = () => {
+import '../../../styles/items/models/Meta.scss'
 
-}
+const Meta = () => {
+    const [meta, setMeta] = useState([])
+    const [erro, setErro] = useState(false)
 
-const Meta = (props) => {
-    console.log('METAS HERE - ')
+    useEffect(() => {
+        const fetchMeta = async () => {
+            try {
+                const fetched = await metaAction.getMeta()
+                setMeta(fetched)
+            } catch (error) {
+                setErro(`Failed to load meta: ${error.message}`)
+            }
+        }
+
+        fetchMeta()
+    }, [])
+
+    return (
+        meta.map(meta => (
+            <div className='meta' key={meta.id}>
+                <div className='head'>
+                    <i className='icon-st fa-solid fa-bullseye'></i><label>{meta.name}</label>
+                </div>
+                <div className='body'></div>
+            </div>
+        ))
+    )
 }
 
 export default Meta
--- reach-goals-app/src/ui/components/pages/Home.jsx
+++ reach-goals-app/src/ui/components/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react'
 import { TitleContext } from '../../../provider/components/TitleProvider'
 
+import Meta from '../items/models/Meta'
+
 import '../../styles/pages/Home.scss'
 
 const Home = () => {
@@ -14,15 +16,25 @@ const Home = () => {
         <div className="container-home">
             <div className="itens">
                 <div className="itens-to-do column">
-                    <div className="head-column"></div>
-                    <div className="body-column"></div>
+                    <div className="head-column">
+                        <label>to do</label>
+                    </div>
+                    <div className="body-column">
+                        <div className='list'>
+                            <Meta/>
+                        </div>
+                    </div>
                 </div>
                 <div className="itens-progress column">
-                    <div className="head-column"></div>
+                    <div className="head-column">
+                        <label>in progress</label>
+                    </div>
                     <div className="body-column"></div>
                 </div>
                 <div className="itens-finished column">
-                    <div className="head-column"></div>
+                    <div className="head-column">
+                        <label>finished</label>
+                    </div>
                     <div className="body-column"></div>
                 </div>
             </div>
