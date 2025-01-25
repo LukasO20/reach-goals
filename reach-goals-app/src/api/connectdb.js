78c7c3cffcee1b7f93e13d7ec135dd8954653b71
Lucas Oliveira <lukinhass2206@outlook.com>
2025-01-25 12:36:20 -0300

    Added some new configurations to new model "Assingment"
    - Improvements to api

--- reach-goals-app/src/api/connectdb.js
+++ reach-goals-app/src/api/connectdb.js
@@ -2,13 +2,12 @@ const { PrismaClient } = require('@prisma/client')
 
 let prisma = undefined
 if (process.env.REACHGOALS_URL === 'production') {
-    prisma = new PrismaClient();
-
+    prisma = new PrismaClient()
 } else {
     if (!global.prisma) {
-      global.prisma = new PrismaClient();
+      global.prisma = new PrismaClient()
     }
-    prisma = global.prisma;
+    prisma = global.prisma
 }
 
 module.exports = prisma
