9aabd81c5b08e266edca708525de0944092fc9ab
Lucas Oliveira <153150990+LukasO20@users.noreply.github.com>
2025-06-11 23:27:35 -0300

    Some little improvements on 'ModalCopy' component

--- reach-goals-app/src/ui/components/items/models/ModelCopy.jsx
+++ reach-goals-app/src/ui/components/items/models/ModelCopy.jsx
@@ -1,6 +1,20 @@
-const ModelCopy = (props) => {
-    //THIS COMPONENT WILL BE A VISUAL REFENCE TO COPY SOME LAYOUT (MAYBE events) OF A REAL MODEL. IT REPLACE THE 'uiLayout.js'
+import { useEffect } from 'react'
 
+//THIS COMPONENT WILL BE A VISUAL REFENCE TO COPY SOME LAYOUT (MAYBE events) OF A REAL MODEL. IT REPLACE THE 'uiLayout.js'
+
+const ModelCopy = ({ model }) => {
+    //The ideia is try to use the hook data 'useGetModel' to push on 'modelsRender' each new model atualized by hook. So 'model' is 'useGetModel'
+    const [modelsRender, setModelsRender] = useState([])
+
+    useEffect(() => {
+        if (model) {
+            setModelsRender([...modelsRender, model])
+        }
+    }, [model])
+
+    return (
+        console.log('MODEL COPY IN ACTION - ', modelsRender)
+    )
 }
 
 export default ModelCopy
