c6c87ba8d2e4398610757e56d633248a743fd811
Lucas Oliveira <153150990+LukasO20@users.noreply.github.com>
2025-08-23 22:39:03 -0300

    add(function): Added new function "reduceModelMap" to centralize logic for render provider data

--- reach-goals-app/src/utils/mapping/mappingUtilsProvider.js
+++ reach-goals-app/src/utils/mapping/mappingUtilsProvider.js
@@ -31,3 +31,24 @@ export const initialStateMap = {
     removed: {},
     saved: {}
 }
+
+export const reduceModelMap = (state, action) => {
+    switch (action.type) {
+        case 'LOADING':
+            return { ...state, loading: true, error: null }
+        case 'FETCH_LIST':
+            return { ...state, loading: false, error: null, data: { ...state.data, core: action.payload } }
+        case 'FETCH_SUPPORT_LIST':
+            return { ...state, loading: false, error: null, data: { ...state.data, support: action.payload } }
+        case 'FETCH_ONE':
+            return { ...state, loading: false, error: null, selected: action.payload }
+        case 'REMOVE_ONE':
+            return { ...state, loading: false, error: null/*, removed: action.payload*/ }
+        case 'SAVE_ONE':
+            return { ...state, loading: false, error: null, saved: action.payload }
+        case 'ERROR':
+            return { loading: false, error: action.payload, data: [] }
+        default:
+            return state
+    }
+}
