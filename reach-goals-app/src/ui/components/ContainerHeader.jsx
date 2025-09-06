cae706dc2cbd61cdc69dac2e8b35714dba7e65d9
Lucas Oliveira <153150990+LukasO20@users.noreply.github.com>
2025-09-05 22:45:41 -0300

    fix(component): fixes problem to open tag panel from header

--- reach-goals-app/src/ui/components/ContainerHeader.jsx
+++ reach-goals-app/src/ui/components/ContainerHeader.jsx
@@ -1,9 +1,9 @@
 import { useContext } from 'react'
-import { useLocation } from 'react-router-dom'
 
 import { ManageModelContext } from '../../provider/ManageModelProvider.jsx'
 import { VisibilityContext } from '../../provider/VisibilityProvider.jsx'
 
+import { useSwitchLayout } from '../../provider/SwitchLayoutProvider.jsx'
 import { useTitle } from '../../provider/TitleProvider.jsx'
 
 import { targetMap, switchLayoutMap } from '../../utils/mapping/mappingUtils.js'
@@ -14,31 +14,31 @@ const ContainerH = () => {
     const { title } = useTitle()
     const { toggleVisibility } = useContext(VisibilityContext)
     const { setModel } = useContext(ManageModelContext)
-    
-    const location = useLocation()
-    const currentLocation = location.pathname.includes('/objectives') ? '/objectives' : location.pathname.includes('/home') ? '/home' : '/calendar'
+    const { layoutComponent } = useSwitchLayout()
 
     const linkTagClick = (event) => {
-        if (event) setModel(prev => ({ ...prev, typeModel: 'tag' }))       
+        if (event) setModel(prev => ({ ...prev, typeModel: 'tag' }))
     }
 
     return (
         <div className="container-header main-content" onClick={(e) => toggleVisibility(targetMap(), e)}>
             <div className="titles-header">
-                <h1>{ title }</h1>
+                <h1>{title}</h1>
             </div>
             <div className="nav">
                 <div className='item-nav'>
-                    <ButtonLink id="btn-theme" classBtn="button-h theme" iconFa="fa-solid fa-palette"/>
+                    <ButtonLink id="btn-theme" classBtn="button-h theme" iconFa="fa-solid fa-palette" />
                 </div>
                 <div className="item-nav" onClick={(e) => { toggleVisibility(targetMap(['panel-right', 'tag']), e); linkTagClick(e) }}>
-                    <ButtonLink id="btn-tag" switchLayout={switchLayoutMap('panel', 'layout', 'right')} link={`${currentLocation}/tag`} classBtn="button-h tag" iconFa="fa-solid fa-tag"/>
+                    <ButtonLink id="btn-tag" switchLayout={switchLayoutMap({ page: layoutComponent.page, name: 'panel', layout: 'layout', value: 'right' })}
+                        link={`/${layoutComponent.page}/tag`} classBtn="button-h tag" iconFa="fa-solid fa-tag"
+                    />
                 </div>
                 {/* <div className="item-nav" onClick={(e) => toggleVisibility(target, e)}>
                     <ButtonLink id="btn-notification" link={`${currentLocation}/notification`} classBtn="button-h notification" iconFa="fa-solid fa-bell"/>
                 </div> */}
                 <div className="item-nav">
-                    <ButtonLink id="btn-profile" classBtn="button-h profile" iconFa="fa-solid fa-user"/>
+                    <ButtonLink id="btn-profile" classBtn="button-h profile" iconFa="fa-solid fa-user" />
                 </div>
             </div>
         </div>
