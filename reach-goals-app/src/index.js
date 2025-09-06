828667b096ee82bf6cf9048b1a6d906f0f290a07
Lucas Oliveira <153150990+LukasO20@users.noreply.github.com>
2025-09-06 19:04:32 -0300

    remove(lib): remove font-awesome

--- reach-goals-app/src/index.js
+++ reach-goals-app/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '../src/app/App.jsx'
-import '../src/lib/fontawesome-free-6.6.0-web/css/all.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
