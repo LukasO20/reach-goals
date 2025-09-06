be98bc371f5053137fcf99ee6572088dc51f60ff
Lucas Oliveira <153150990+LukasO20@users.noreply.github.com>
2025-09-06 00:25:48 -0300

    improve(components): ButtonLink renders icon and images

--- reach-goals-app/src/ui/components/items/elements/ButtonLink.jsx
+++ reach-goals-app/src/ui/components/items/elements/ButtonLink.jsx
@@ -2,16 +2,21 @@ import { Link } from 'react-router-dom'
 
 import { useSwitchLayout } from '../../../../provider/SwitchLayoutProvider.jsx'
 
+import { iconMap } from '../../../../utils/mapping/mappingUtils.js'
+
 const ButtonLink = (props) => {
-    const { switchLayoutComponent } = useSwitchLayout() 
+    const { switchLayoutComponent } = useSwitchLayout()
 
     const handleClick = (e) => {
         if (props.switchLayout) { switchLayoutComponent(props.switchLayout) }
     }
 
     return (
-        <Link to={`${props.link !== undefined ? props.link : '/' }`} onClick={handleClick}>
-            <span className={`${props.classBtn} button-st`}><i className={`icon-st ${props.iconFa}`}></i></span>
+        <Link to={`${props.link !== undefined ? props.link : '/'}`} className={props.classBtn} onClick={handleClick}>
+            {props.img !== undefined ?
+                <img src={props.img} alt={props.imgAlt} /> :
+                <span className={`${props.classBtn} button-st`}>{iconMap[props.icon]}</span>
+            }
         </Link>
     )
 }
