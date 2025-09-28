ae8824f5356f6041e89653caf35282caa5ef8b74
Lucas de Oliveira Silva <153150990+LukasO20@users.noreply.github.com>
2025-09-27 22:56:04 -0300

    add(util): add new util reference.js to help with types

---
+++ reach-goals-app/src/utils/reference.js
@@ -0,0 +1,8 @@
+export const typeModel = ['goal', 'tag', 'assignment']
+
+export const typeReduceModel = ['core', 'support']
+
+export const typeFilterModel = ['goalSomeID', 'goalAssignmentRelation', 'goalTagRelation',
+    'notAssignmentRelation', 'assignmentSomeID', 'assignmentGoalRelation',
+    'assignmentTagRelation', 'notGoalRelation', 'tagSomeID',
+    'tagsRelation', 'tagsNotRelation']
