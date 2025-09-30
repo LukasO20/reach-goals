68500b55f450f22e2782ee030cf13e7ea8e1c08b
Lucas Oliveira <153150990+LukasO20@users.noreply.github.com>
2025-09-29 23:40:31 -0300

    update(component): add new function plusProps to help share extra props to children component
    - Initial ModelSwitcher uses plusProps to render correct props

--- reach-goals-app/src/ui/components/panels/ModalList/ModalList.jsx
+++ reach-goals-app/src/ui/components/panels/ModalList/ModalList.jsx
@@ -5,23 +5,49 @@ import { modalListMap } from '../../../../utils/mapping/mappingUtils.js'
 
 import './ModalList.scss'
 
+//Plus props can be used to add extra properties to the component
+const plusProps = (props) => {
+    const from = props?.from ?? null
+
+    let switcherRelation = null
+    if (props?.type === 'goal') switcherRelation = 'goal-relation'
+    else if (props?.type === 'assignment') switcherRelation = 'assignment-relation'
+
+    switch (from) {
+        case 'form':
+            const currentFilter = props?.type === 'goal'  ? { notGoalRelation: 'all' } : { goalSomeID: 'all' }
+
+            return {
+                propsReference: {
+                    ...currentFilter,
+                    typeDataSource: 'support',
+                },
+                switcherRelationType: switcherRelation
+            }
+        default:
+            return {
+                propsReference: null,
+                switcherRelationType: null
+            }
+    }
+}
+
 const ModalList = (props) => {
     const title = props?.title
+    const type = plusProps(props).switcherRelationType
+    const propsReference = plusProps(props).propsReference
     const externalFunction = props?.exFunction
-    const externalRequestProps = props?.externalRequestProps
-
-    let complement = props?.complement
-    if (complement === 'goal') { complement = 'goal-relation' }
-    else if (complement === 'assignment') { complement = 'assignment-relation' }
 
     return (
-        <div className={`container-list-modal ${complement}`}>
+        <div className={`container-list-modal ${props.type}`}>
             <div className='head'>
                 <h2>{title}</h2>
-                <ButtonAction modalList={modalListMap(false)} standardRoute='true' classBtn='button-action circle close' icon='close'/>
+                <ButtonAction modalList={modalListMap(false)} standardRoute='true' classBtn='button-action circle close' icon='close' />
             </div>
             <div className='body scrollable'>
-                {<ModelSwitcher type={complement} selectableModel={true} action={{ setForm: true }} propsReference={externalRequestProps} exFunction={externalFunction} />}
+                {/* ModelSwitcher here is ready to render a relation Model */}
+                {<ModelSwitcher type={type} selectableModel={true} action={{ setForm: true }}
+                    propsReference={propsReference} exFunction={externalFunction} />}
             </div>
         </div>
     )
