1f29a4c28adc31ea67a50a2dc3dd5e6568656be9
Lucas de Oliveira Silva <153150990+LukasO20@users.noreply.github.com>
2025-10-05 19:41:28 -0300

    update and refactor(component and style): change handle function's action and add more class to ModalTag
    - This update affect the component according new styles

--- reach-goals-app/src/ui/components/panels/ModalTag/ModalTag.jsx
+++ reach-goals-app/src/ui/components/panels/ModalTag/ModalTag.jsx
@@ -1,23 +1,31 @@
 import { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import { VisibilityContext } from '../../../../provider/VisibilityProvider.jsx'
 import { ManageModelContext } from '../../../../provider/ManageModelProvider.jsx'
 
-import { targetMap, checkboxMap } from '../../../../utils/mapping/mappingUtils.js'
+import { useSwitchLayout } from '../../../../provider/SwitchLayoutProvider.jsx'
+
+import { targetMap, checkboxMap, switchLayoutMap } from '../../../../utils/mapping/mappingUtils.js'
 
 import Tag from '../../items/models/Tag/Tag.jsx'
 import ModalForm from '../ModalForm/ModalForm.jsx'
 import ButtonAction from '../../items/elements/ButtonAction/ButtonAction.jsx'
 import ButtonCheckbox from '../../items/elements/ButtonCheckbox/ButtonCheckbox.jsx'
 
+import './ModalTag.scss'
+
 const ModalTag = () => {
     const { visibleElements } = useContext(VisibilityContext)
     const { setModel } = useContext(ManageModelContext)
+    const { layoutComponent } = useSwitchLayout()
+    const navigate = useNavigate()
 
     const isModalForm = ['tag', 'near-modalForm']
 
-    const configClick = (event) => {
-        if (event) setModel(prev => ({ ...prev, typeModel: 'config' }))     
+    const handleClickConfig = (e) => {
+        if (e) setModel(prev => ({ ...prev, typeModel: 'config' }))  
+        navigate(`/${layoutComponent.page}`) // return standard route during handle   
     }
 
     return (
@@ -26,11 +34,12 @@ const ModalTag = () => {
                 <h2>Tags</h2>
                 <ButtonAction target={targetMap(null)} standardRoute="true" classBtn='button-action circle close' icon='close'/>
                 <div className='options'>
-                    <ButtonAction target={targetMap('near-modalForm', { add: true })} classBtn='form-tag btn-option-r create' icon='plus' title='create'/>
-                    <ButtonCheckbox checkbox={checkboxMap({ id: 'checkbox-r-tag', value: false })} classBtn='checkbox-r-tag btn-checkbox'/>
+                    <ButtonAction target={targetMap('near-modalForm', { add: true })} classBtn='button-action create plan max-width' icon='plus' title='create'/>
+                    {/* <ButtonCheckbox checkbox={checkboxMap({ id: 'checkbox-r-tag', value: false })} classBtn='checkbox-r-tag btn-checkbox'/> */}
                     {/* <ButtonDropdown target={targetMap('btn-filter-tag', { add: true })} classBtn='btn-option-r filter-content' iconFa='fa-solid fa-filter'/> */}
                     {/* Maybe a search bar can be better than a filter button, consider this aproach */}
-                    <ButtonAction target={targetMap(['panel-center', 'config'])} classBtn='btn-option-r config-content' icon='config' onClick={(e) => configClick(e)}/>
+                    <ButtonAction classBtn='button-action circle max-width config' icon='config' onClick={(e) => handleClickConfig(e)} 
+                        target={targetMap(['panel-center', 'config'])} switchLayout={switchLayoutMap({ page: layoutComponent.page, name: 'panel', layout: 'layout', value: 'center' })}/>
                 </div>
             </div>
             <div className='body'>
