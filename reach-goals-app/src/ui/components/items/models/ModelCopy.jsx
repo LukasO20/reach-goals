1b77724ec5b625b13c1c0395d7a22c18666dd750
Lucas Oliveira <153150990+LukasO20@users.noreply.github.com>
2025-10-08 00:31:51 -0300

    update(component): improve function handleModelCopyClick to new name and structure
    - handleModelCopyClick is called removeElDOMClick now to follow nomenclatures

--- reach-goals-app/src/ui/components/items/models/ModelCopy.jsx
+++ reach-goals-app/src/ui/components/items/models/ModelCopy.jsx
@@ -17,31 +17,26 @@ const ModelCopy = ({ displayRef, region }) => {
         setCopyModel(model.transportModel[region] || [])
     }, [model.transportModel])
 
-    const handleModelCopyClick = (modelID, action, type) => {
+    const removeElDOMClick = ({ id, tagID }, type) => {
+        removeFromTransportModel({ id: id ?? tagID, type: type })
 
-        switch (action) {
-            case 'delete': //Right now, only delete action is available
-                removeFromTransportModel({ id: modelID, type: type })
+        if (!type) { return console.error('No type provided for model copy delete action. Send a goal, assignment or tag type') }
 
-                if (!type) { return console.error('No type provided for model copy delete action') }
-
-                switch (type) {
-                    case 'goal':
-                        updateSubmitModel({ keyObject: 'goalID', value: null, action: 'remove'})
-                        break
-                    case 'assignment':
-                        updateSubmitModel({ keyObject: 'assignments', value: { id: modelID }, type: 'array', action: 'remove' })
-                        break
-                    case 'tag':
-                        updateSubmitModel({ keyObject: 'tags', value: { tagID: modelID }, type: 'array', action: 'remove' })
-                }
+        switch (type) {
+            case 'goal':
+                updateSubmitModel({ keyObject: 'goalID', value: null, action: 'remove' })
                 break
+            case 'assignment':
+                updateSubmitModel({ keyObject: 'assignments', value: { id: id }, type: 'array', action: 'remove' })
+                break
+            case 'tag':
+                updateSubmitModel({ keyObject: 'tags', value: { tagID: tagID }, type: 'array', action: 'remove' })
         }
     }
 
     const clickEvents = {
         card: () => { },
-        aux: handleModelCopyClick
+        aux: removeElDOMClick
     }
 
     return <CardItem type={modelCopy[0]?.type} model={modelCopy} clickFunction={clickEvents} display={display} />
