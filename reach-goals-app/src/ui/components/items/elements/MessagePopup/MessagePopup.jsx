5915d367c2e2cd5e3de5dece232ffaf05000ec8c
Lucas de Oliveira Silva <153150990+LukasO20@users.noreply.github.com>
2025-10-12 19:21:57 -0300

    remove(logic): removes MessagePopup main logic

--- reach-goals-app/src/ui/components/items/elements/MessagePopup/MessagePopup.jsx
+++ reach-goals-app/src/ui/components/items/elements/MessagePopup/MessagePopup.jsx
@@ -6,28 +6,7 @@ import { iconMap, targetMap } from '../../../../../utils/mapping/mappingUtils'
 
 const MessagePopup = (props) => {
     const { visibleElements, toggleVisibility } = useContext(VisibilityContext)
-    const message = props?.message
-    const timer = typeof props?.timer !== 'number' ? 5000 : props?.timer
-    const messagePopupReady = visibleElements.includes('message-popup')
-
-    // toggleVisibility(targetMap('message-popup', { add: true }))
-
-    // useEffect(() => {
-    //     const timeout = setTimeout(() => {
-    //         toggleVisibility(targetMap('message-popup', { remove: true }))    
-    //     }, timer)
-
-    //     return () => clearTimeout(timeout)  
-    // }, [timer])
-
-    return (
-        typeof message === 'string' && message !== '' && messagePopupReady ?
-            <div className='message-popup'>
-                <span>{message}</span>
-                {iconMap['close']}
-            </div>
-            : null
-    )
+    return null
 }
 
 export default MessagePopup
