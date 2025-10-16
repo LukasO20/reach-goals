5f4874c2e91285a0770270fc25c3156a2d71f9f1
Lucas Oliveira <153150990+LukasO20@users.noreply.github.com>
2025-10-15 23:39:43 -0300

    update(component): update MonthDaysPicker to render calendar layout

--- reach-goals-app/src/ui/components/items/elements/MonthDaysPicker/MonthDaysPicker.jsx
+++ reach-goals-app/src/ui/components/items/elements/MonthDaysPicker/MonthDaysPicker.jsx
@@ -1,17 +1,11 @@
 import { useState } from 'react'
+import { weekNames } from '../../../../../utils/reference.js'
+
+import './MonthDaysPicker.scss'
 
 const MonthDaysPicker = (props) => {
     const [year] = useState(new Date().getFullYear())
     const [month, setMonth] = useState(new Date().getMonth())
-
-    const monthNames = [
-        'January', 'February', 'March', 'April', 'May', 'June',
-        'July', 'August', 'September', 'October', 'November', 'December',
-    ]
-
-    const weekNames = [
-        'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
-    ]
 
     const getDaysInMonth = (year, month) => {
         const date = new Date(year, month, 1)
@@ -24,17 +18,44 @@ const MonthDaysPicker = (props) => {
     }
 
     const days = getDaysInMonth(year, month)
+    const models = props.model || { goal: [], assignment: [] }
+
+    const modelsCalendar = Object.entries(models)
+        .map(([key, value]) =>
+            value.core.map(item => ({ ...item, type: key }))
+        )
+        .flat()
 
     return (
-        days.map(day => {
-            const weekdate = weekNames[day.getDay()]
-            const daydate = day.getDate()
-
-            return (
-                <div>
-                </div>
-            )
-        })
+        <div className='calendar'>
+            <div className='header'>
+                {
+                    weekNames.map(week => {
+                        return (
+                            <div key={week} className='day-name'>{week}</div>
+                        )
+                    })
+                }
+            </div>
+            <div className='body'>
+                {
+                    //Fill with empty day to align first day of month
+                    Array(days[0].getDay()).fill(null).map((_, i) => (
+                        <div key={`empty-${i}`} className='day empty'></div>
+                    ))
+                }
+                {
+                    //Render actual days
+                    days.map(day => (
+                        <div key={day.toISOString()} day={day.getDate()} className={`day ${new Date().toDateString() === day.toDateString() ? 'today' : ''}`}>
+                            <span className='title'>
+                                {day.getDate()}
+                            </span>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
     )
 }
 
