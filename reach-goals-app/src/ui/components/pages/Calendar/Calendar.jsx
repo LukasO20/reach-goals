6c6c4fd0cc56e22c1b7ae1484eeaac041bf7b9b5
Lucas Oliveira <153150990+LukasO20@users.noreply.github.com>
2025-10-15 23:42:06 -0300

    update(component page): update Calendar to share model data

--- reach-goals-app/src/ui/components/pages/Calendar/Calendar.jsx
+++ reach-goals-app/src/ui/components/pages/Calendar/Calendar.jsx
@@ -1,19 +1,50 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useTitle } from '../../../../provider/TitleProvider.jsx'
+
+import { useGoalModel } from '../../../../provider/model/GoalModelProvider.jsx'
+import { useAssignmentModel } from '../../../../provider/model/AssignmentModelProvider.jsx'
+
+import { filterGetModel } from '../../../../utils/mapping/mappingUtils.js'
 
 import MonthDaysPicker from '../../items/elements/MonthDaysPicker/MonthDaysPicker.jsx'
 
 const Calendar = () => {
     const { update } = useTitle()
+    const { data: dataGoal, refetch: refetchGoal } = useGoalModel()
+    const { data: dataAssignment, refetch: refetchAssignment } = useAssignmentModel()
+    const [activeModelSource, setActiveModelSource] = useState({
+        goal: [],
+        assignment: []
+    })
+
+    const filterGetGoal = filterGetModel({ goalSomeID: 'all' }, 'goal', 'core')
+    const filterGetAssignment = filterGetModel({ notGoalRelation: 'all' }, 'assignment', 'core')
 
     useEffect(() => {
-        update({ header: 'Manage your goals and assingments' })
+        update({ header: 'Manage your goals and assignments' })
+
+        const fetch = async () => {
+            await refetchGoal(filterGetGoal)
+            await refetchAssignment(filterGetAssignment)
+        }
+
+        fetch()
     }, [])
+
+    useEffect(() => {
+        setActiveModelSource(prevModel => (
+            {
+                ...prevModel,
+                goal: dataGoal ?? [],
+                assignment: dataAssignment ?? []
+            }
+        ))
+    }, [dataGoal, dataAssignment])
 
     return (
         <div className="container-calendar">
-            <MonthDaysPicker />
-        </div> 
+            <MonthDaysPicker model={activeModelSource} />
+        </div>
     )
 }
 
