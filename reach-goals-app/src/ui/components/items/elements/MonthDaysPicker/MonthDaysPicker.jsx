81b9e17ada269c50dcb1487122729db6c9ecc453
Lucas Oliveira <153150990+LukasO20@users.noreply.github.com>
2025-10-17 00:01:17 -0300

    update(component): improve styles and render clicable card on MonthDaysPicker's calendar container
    - Set details model to each card
    - Render a model according calendar date

--- reach-goals-app/src/ui/components/items/elements/MonthDaysPicker/MonthDaysPicker.jsx
+++ reach-goals-app/src/ui/components/items/elements/MonthDaysPicker/MonthDaysPicker.jsx
@@ -1,11 +1,23 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
+
+import { ManageModelContext } from '../../../../../provider/ManageModelProvider.jsx'
+import { VisibilityContext } from '../../../../../provider/VisibilityProvider.jsx'
+
 import { weekNames } from '../../../../../utils/reference.js'
+import { formatDate } from '../../../../../utils/utils.js'
+import { switchLayoutMap, targetMap } from '../../../../../utils/mapping/mappingUtils.js'
 
 import './MonthDaysPicker.scss'
+
+import moment from 'moment'
+import { useSwitchLayout } from '../../../../../provider/SwitchLayoutProvider.jsx'
 
 const MonthDaysPicker = (props) => {
     const [year] = useState(new Date().getFullYear())
     const [month, setMonth] = useState(new Date().getMonth())
+    const { setModel } = useContext(ManageModelContext)
+    const { toggleVisibility } = useContext(VisibilityContext)
+    const { layoutComponent, switchLayoutComponent } = useSwitchLayout()
 
     const getDaysInMonth = (year, month) => {
         const date = new Date(year, month, 1)
@@ -17,14 +29,26 @@ const MonthDaysPicker = (props) => {
         return days
     }
 
+    const activityClick = ({ id, type }) => {
+        setModel(prev => ({ ...prev, mainModelID: id, typeModel: type }))
+        switchLayoutComponent(switchLayoutMap({ page: layoutComponent.page, name: 'panel', layout: 'layout', value: 'right' }))
+        toggleVisibility(targetMap(['panel-right', type]))
+    }
+
     const days = getDaysInMonth(year, month)
     const models = props.model || { goal: [], assignment: [] }
 
-    const modelsCalendar = Object.entries(models)
-        .map(([key, value]) =>
-            value.core.map(item => ({ ...item, type: key }))
-        )
-        .flat()
+    const modelsCalendar = Object.entries(models || {}).flatMap(([key, value]) =>
+        Array.isArray(value?.core) ?
+            value.core.map(model => (
+                {
+                    ...model,
+                    type: key,
+                    start: formatDate(model).start,
+                    end: formatDate(model).end,
+                }
+            )) : []
+    )
 
     return (
         <div className='calendar'>
@@ -46,11 +70,22 @@ const MonthDaysPicker = (props) => {
                 }
                 {
                     //Render actual days
-                    days.map(day => (
+                    days.map(day => (                       
                         <div key={day.toISOString()} day={day.getDate()} className={`day ${new Date().toDateString() === day.toDateString() ? 'today' : ''}`}>
                             <span className='title'>
                                 {day.getDate()}
                             </span>
+                            {
+                                modelsCalendar.map(model => {
+                                    //Filter models that start on this day
+                                    return (
+                                        model.start ===  moment(day).format('DD/MM/YYYY') &&
+                                        <div key={model.id} className='activity' onClick={() => activityClick(model)}>
+                                            <label className='activity-name'>{model.name}</label>                                           
+                                        </div>
+                                    )
+                                })
+                            }
                         </div>
                     ))
                 }
