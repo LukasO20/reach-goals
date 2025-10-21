67be574afa178491737ff47cefaf6b2996ff8178
Lucas Oliveira <153150990+LukasO20@users.noreply.github.com>
2025-10-21 00:15:50 -0300

    add(new provider): add FilterModelProvider
    - FilterModelProvider help to centralize use of ManageModelProvider'filter to share

---
+++ reach-goals-app/src/provider/model/FilterModelProvider.jsx
@@ -0,0 +1,25 @@
+import { useContext } from 'react'
+
+import { GoalModelProvider } from './GoalModelProvider.jsx'
+import { AssignmentModelProvider } from './AssignmentModelProvider.jsx'
+import { TagModelProvider } from './TagModelProvider.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ManageModelContext } from '../ManageModelProvider.jsx'
+
+const queryClient = new QueryClient()
+
+export const FilterModelProvider = ({ children }) => {
+    const { model } = useContext(ManageModelContext)
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <GoalModelProvider filters={model.filter.goal}>
+                <AssignmentModelProvider>
+                    <TagModelProvider>
+                        {children}
+                    </TagModelProvider>
+                </AssignmentModelProvider>
+            </GoalModelProvider>
+        </QueryClientProvider>
+    )
+}
