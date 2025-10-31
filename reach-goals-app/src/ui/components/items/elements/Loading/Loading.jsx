f6738b27934c0b4d6175f141f91f12fb37892d8e
Lucas Oliveira <153150990+LukasO20@users.noreply.github.com>
2025-10-31 00:27:47 -0300

    add: new component Loading

---
+++ reach-goals-app/src/ui/components/items/elements/Loading/Loading.jsx
@@ -0,0 +1,11 @@
+import '../Loading/Loading.scss'
+
+const Loading = (props) => {
+    return (
+        <div className="spinner-overlay">
+            <div className="spinner"></div>
+        </div>
+    )
+}
+
+export default Loading
