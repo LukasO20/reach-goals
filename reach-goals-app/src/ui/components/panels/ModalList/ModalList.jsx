80bcb93996bb0606c2df12801259362679b844a5
Lucas Oliveira <153150990+LukasO20@users.noreply.github.com>
2025-11-11 23:19:12 -0300

    update(component): add support to use updateModelFiter and new styles on ModalList

--- reach-goals-app/src/ui/components/panels/ModalList/ModalList.jsx
+++ reach-goals-app/src/ui/components/panels/ModalList/ModalList.jsx
@@ -1,40 +1,53 @@
 import { useEffect, useContext } from 'react'
 
 import { ManageModelContext } from '../../../../provider/ManageModelProvider.jsx'
+import { useGoalProvider } from '../../../../provider/model/GoalModelProvider.jsx'
+import { useAssignmentProvider } from '../../../../provider/model/AssignmentModelProvider.jsx'
+import { useTagProvider } from '../../../../provider/model/TagModelProvider.jsx'
 
 import { filterGetModelMap, targetMap } from '../../../../utils/mapping/mappingUtils.js'
 
 import ModelSwitcher from '../../items/models/ModelSwitcher.jsx'
 import ButtonAction from '../../items/elements/ButtonAction/ButtonAction.jsx'
+import Loading from '../../items/elements/Loading/Loading.jsx'
 
 import './ModalList.scss'
 
 const ModalList = (props) => {
     const { updateFilterModel } = useContext(ManageModelContext)
+    const { panel: { loading: loadingGoal } } = useGoalProvider()
+    const { panel: { loading: loadingAssigment } } = useAssignmentProvider()
+    const { panel: { loading: loadingTag } } = useTagProvider()
+
     const type = props.type
     const title = props.title
-    const visibilityRelation = type === 'tag' ? 'tag' : type === 'goal' ? 'assignment' : 'goal'
-
-    const filterGetModel = filterGetModelMap({
-        notGoalRelation: 'all',
-        type: 'assignment',
-        source: 'support'
-    }, 'assignment', 'support')
-
+    const typeRelation = type === 'tag' ? 'tag' : type === 'goal' ? 'assignment' : 'goal'
+    const typeSwitcherRelation = type === 'tag' ? 'tag' : type === 'goal' ? 'goal-relation' : 'assignment-relation'
 
     useEffect(() => {
-        updateFilterModel(filterGetModel, 'assignment', 'panel')
+        const typeKey = typeRelation === 'tag' ? 'tagSomeID' : type === 'goal' ? 'notGoalRelation' : 'goalSomeID'
+        const filterGetModel = filterGetModelMap({
+            [typeKey]: 'all',
+            type: typeRelation,
+            source: 'support'
+        }, typeRelation, 'support')
+
+        updateFilterModel(filterGetModel, typeRelation, 'panel')
     }, [])
 
     return (
         <div className={`container-list-modal ${type}`}>
             <div className='head'>
                 <h2>{title}</h2>
-                <ButtonAction target={targetMap(`modal-list-${visibilityRelation}`, { remove: true })}
-                 standardRoute='true' classBtn='button-action circle close' icon='close' />
+                <ButtonAction target={targetMap(`modal-list-${typeRelation}`, { remove: true })}
+                    standardRoute='true' classBtn='button-action circle close' icon='close' />
             </div>
             <div className='body scrollable'>
-                <ModelSwitcher type='goal-relation' />
+                {
+                    (loadingGoal || loadingAssigment || loadingTag) ?
+                        <Loading />
+                        :
+                        <ModelSwitcher type={typeSwitcherRelation} />}
             </div>
         </div>
     )
