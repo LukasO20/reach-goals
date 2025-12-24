5dfadac08daaf93a69f837d2430d8d179a4893ff
Lucas Oliveira <153150990+LukasO20@users.noreply.github.com>
2025-12-24 00:27:22 -0300

    refactor(routes): remove obsolete logic to render a modal component

--- reach-goals-app/src/app/Routes.jsx
+++ reach-goals-app/src/app/Routes.jsx
@@ -1,21 +1,12 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 
-import { useSwitchLayout } from '../provider/SwitchLayoutProvider.jsx'
-
 import Sections from '../ui/components/SectionLayout'
-import ModalSwitcherRight from '../ui/components/modals/ModalSwitcherRight.jsx'
 
 const AppRoutes = () => {
-    const { layoutComponent } = useSwitchLayout()
-
-    const typeLayout = layoutComponent.panel.layout
-
     return (
         <Routes>
             <Route path='/' element={<Navigate to='/home' />} />
-            <Route path='/:section' element={<Sections />}>
-                <Route path={`modal-right/${typeLayout}`} element={<ModalSwitcherRight />} />
-            </Route>
+            <Route path='/:section' element={<Sections />} />
         </Routes>
     )
 }
